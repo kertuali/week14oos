deff50ba7462a674371380286fcf4db7b73c269b
KKertu Ali <kertu.greenberg@tktk.ee>
2023-11-09 19:54:20 +0200

    week14

---
+++ week14OOP/Program.cs
@@ -0,0 +1,63 @@
+﻿Console.WriteLine("name your dog");
+string myDogName = Console.ReadLine();
+Dog myDog = new Dog(myDogName);
+
+
+Dog neighboursDog = new Dog("Good girl");
+
+
+Console.WriteLine($"My dogs name is {myDog.Name}.");
+Console.WriteLine($"My neoghbours dogs name is {neighboursDog.Name}");
+
+myDog.Rename("Bad boy");
+
+while (myDog.LevelOfHappiness != 5)
+{ 
+    myDog.Bark();
+}
+
+myDog.WagTail();
+
+class Dog
+{
+    private string _name; //field
+    private int _levelOfHappiness;
+
+    //constructor
+    public Dog(string name) //name - let the user name the dog
+    {
+        _name = name;
+        _levelOfHappiness = 0;
+
+        
+    }
+
+    //getter
+
+    public string Name
+    {
+        get { return _name; }  
+    }
+
+    public int LevelOfHappiness
+    {
+        get { return _levelOfHappiness; }
+    }
+
+    public void Rename(string newName)
+    {
+        _name = newName;
+        Console.WriteLine($"The dog has been renamed to {newName}");
+    }
+
+    public void Bark()
+    { 
+        Console.WriteLine("Woof");
+        _levelOfHappiness++;
+    }
+
+    public void WagTail()
+    { 
+        Console.WriteLine("Wiggle-wiggle");
+    }
+}
